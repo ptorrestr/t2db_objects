b7720a0b924acbc0380cf2665e140631be655572
Pablo Torres <pablo.torres@deri.org>
2013-09-26 18:13:56 +0100

    change in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = 't2db_objects',
-    version = '0.1',
+    version = '0.2.1',
     description = 'Bases objects for t2db',
     long_description = readme(),
     classifiers = [
