f66dae6a3e4f409b4ae22ba9b53870888d307e38
Pablo Torres <pablo.torres@deri.org>
2013-09-26 16:37:41 +0100

    order dictory for python setuptool

--- setup.py
+++ setup.py
@@ -4,24 +4,21 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='t2db_objects',
-      version='0.1',
-      description='Bases objects for t2db',
-      long_description=readme(),
-      classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.2',
-      ],
-      url='http://github.com/ptorrest/t2db_objects',
-      author='Pablo Torres',
-      author_email='pablo.torres@deri.org',
-      license='GNU',
-      packages=['t2db_objects'],
-      install_requires=[
-      ],
-      test_suite='t2db_objects.tests',
-      #tests_require=['nose', 'nose-cover3'],
-      #entry_points={
-      #    'console_scripts': ['funniest-joke=funniest.cmd:main'],
-      #},
-      zip_safe=False)
+setup(
+    name = 't2db_objects',
+    version = '0.1',
+    description = 'Bases objects for t2db',
+    long_description = readme(),
+    classifiers = [
+       'Programming Language :: Python :: 3.2',
+    ],
+    url = 'http://github.com/ptorrest/t2db_objects',
+    author = 'Pablo Torres',
+    author_email = 'pablo.torres@deri.org',
+    license = 'GNU',
+    packages = ['t2db_objects'],
+    install_requires = [
+    ],
+    test_suite = 't2db_objects.tests',
+    zip_safe = False,
+)
