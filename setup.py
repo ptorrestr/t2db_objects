a01862ec0ba7ee4fd12701b2530e8f1167cc6915
Pablo Torres <pablo.torres@deri.org>
2013-09-30 14:53:50 +0100

    new version 0.5: Added Daemon object which allows to daemonise other object just extending the declaration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = 't2db_objects',
-    version = '0.4.2',
+    version = '0.5.0',
     description = 'Bases objects for t2db',
     long_description = readme(),
     classifiers = [
