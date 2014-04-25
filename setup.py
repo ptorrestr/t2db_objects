3024a6437219cd32c7e417213b69c6fd73127ac8
Pablo Torres <pablo.torres@deri.org>
2014-04-25 19:54:06 +0100

    add examples files for XSV parsing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = 't2db_objects',
-    version = '0.5.9',
+    version = '0.5.10',
     description = 'Bases objects for t2db',
     long_description = readme(),
     classifiers = [
