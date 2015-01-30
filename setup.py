a5ffec79da98dcd888fc6468467d4ec05ff6e1e0
Pablo Torres <pablo.torres@deri.org>
2015-01-30 15:47:08 +0000

    new version 0.6.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = 't2db_objects',
-    version = '0.6',
+    version = '0.6.1',
     description = 'Bases objects for t2db',
     long_description = readme(),
     classifiers = [
