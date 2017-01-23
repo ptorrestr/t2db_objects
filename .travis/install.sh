257633fce7825e5929dd1ef0c50b7d92433c1324
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-23 16:29:24 +0000

    install conda-build

--- .travis/install.sh
+++ .travis/install.sh
@@ -28,5 +28,6 @@ conda config --add channels ptorrestr
 conda config --append channels pkgw
 conda config --get channels
 conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
+conda install conda-build
 source activate test-environment
 conda build .conda/ --no-test
